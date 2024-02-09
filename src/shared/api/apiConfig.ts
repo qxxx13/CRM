973dc26fd900f4014915b49301d4dc7fbd3d598b
Nikolay Nazarenko <redecv9@gmail.com>
2024-02-09 21:41:37 +0300

    fix: url

--- src/shared/api/apiConfig.ts
+++ src/shared/api/apiConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //TODO authToken from props
 export const instance = axios.create({
-    baseURL: 'http://localhost:5555/api/',
+    baseURL: 'http://77.91.84.85:5555/api/',
     headers: {
         'Content-Type': 'application/json',
     },
