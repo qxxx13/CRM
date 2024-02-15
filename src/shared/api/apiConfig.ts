e1306ff41a92b2ed67887f407464e73516e7836f
Nikolay Nazarenko <redecv9@gmail.com>
2024-02-16 01:14:05 +0300

    fix: url

--- src/shared/api/apiConfig.ts
+++ src/shared/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:5555/api/',
+    baseURL: 'http://77.91.84.85:5555/api/',
     headers: {
         'Content-Type': 'application/json',
     },
